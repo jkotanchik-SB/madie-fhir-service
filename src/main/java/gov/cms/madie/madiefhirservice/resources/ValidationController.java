5ed11f6fc419642c974850ffcd2515b545e8f862
Nate Moraca <nate.moraca@semanticbits.com>
2022-08-25 16:35:08 -0400

    add new endpoint for test case bundle validation

---
+++ src/main/java/gov/cms/madie/madiefhirservice/resources/ValidationController.java
@@ -0,0 +1,122 @@
+package gov.cms.madie.madiefhirservice.resources;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.DataFormatException;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.parser.LenientErrorHandler;
+import ca.uhn.fhir.validation.FhirValidator;
+import ca.uhn.fhir.validation.IValidatorModule;
+import ca.uhn.fhir.validation.ValidationResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.cms.madie.models.measure.HapiOperationOutcome;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
+import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.OperationOutcome;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/fhir/validations")
+@Tag(name = "HAPI-FHIR-Validation-Controller", description = "API for validating resources using HAPI utilities")
+@AllArgsConstructor
+public class ValidationController {
+
+  private FhirContext fhirContext;
+  private ValidationSupportChain validationSupportChain;
+
+  @PostMapping(
+      path = "/bundles",
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public HapiOperationOutcome validateBundle(HttpEntity<String> request) {
+    LenientErrorHandler lenientErrorHandler = new LenientErrorHandler().setErrorOnInvalidValue(false);
+    IParser parser = fhirContext.newJsonParser().setParserErrorHandler(lenientErrorHandler).setPrettyPrint(true);
+    IBaseResource resource;
+    try {
+      resource = parser.parseResource(request.getBody());
+    } catch (DataFormatException ex) {
+      OperationOutcome operationOutcome = new OperationOutcome();
+      operationOutcome
+          .addIssue()
+          .setSeverity(OperationOutcome.IssueSeverity.ERROR)
+          .setCode(OperationOutcome.IssueType.INVALID)
+          .setDiagnostics(ex.getMessage());
+      return encodeOutcome(
+          parser,
+          HttpStatus.BAD_REQUEST.value(),
+          false,
+          "An error occurred while parsing the resource",
+          operationOutcome
+      );
+    }
+
+    // only validate bundles
+    if (!"BUNDLE".equalsIgnoreCase(resource.fhirType())) {
+      OperationOutcome operationOutcome = new OperationOutcome();
+      operationOutcome
+          .addIssue()
+          .setSeverity(OperationOutcome.IssueSeverity.ERROR)
+          .setCode(OperationOutcome.IssueType.INVALID)
+          .setDiagnostics("resourceType must be 'Bundle'");
+      return encodeOutcome(
+          parser,
+          HttpStatus.BAD_REQUEST.value(),
+          false,
+          "Resource must have resourceType of 'Bundle'",
+          operationOutcome
+      );
+    }
+
+    // Ask the context for a validator
+    FhirValidator validator = fhirContext.newValidator();
+
+    // Create a validation module and register it
+    IValidatorModule module = new FhirInstanceValidator(validationSupportChain);
+    validator.registerValidatorModule(module);
+    ValidationResult result = validator.validateWithResult(resource);
+    String outcomeString = parser.encodeResourceToString(result.toOperationOutcome());
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      return HapiOperationOutcome.builder()
+          .code(HttpStatus.OK.value())
+          .successful(result.isSuccessful())
+          .outcomeResponse(mapper.readValue(outcomeString, Object.class))
+          .build();
+    } catch (JsonProcessingException jpe) {
+      throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  protected HapiOperationOutcome encodeOutcome(
+      IParser parser,
+      int code,
+      boolean successful,
+      String message,
+      OperationOutcome outcome
+  ) {
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      String outcomeString = parser.encodeResourceToString(outcome);
+      return HapiOperationOutcome.builder()
+          .code(code)
+          .message(message)
+          .successful(successful)
+          .outcomeResponse(mapper.readValue(outcomeString, Object.class))
+          .build();
+    } catch (JsonProcessingException jpe) {
+      throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
+    }
+  }
+
+}
---
+++ src/test/java/gov/cms/madie/madiefhirservice/resources/ValidationControllerMvcTest.java
@@ -0,0 +1,104 @@
+package gov.cms.madie.madiefhirservice.resources;
+
+import ca.uhn.fhir.context.FhirContext;
+import gov.cms.madie.madiefhirservice.utils.ResourceFileUtil;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({ValidationController.class})
+@Import(ValidationMvcTestConfiguration.class)
+@ActiveProfiles("MvcTest")
+class ValidationControllerMvcTest implements ResourceFileUtil {
+  private static final String TEST_USER_ID = "john_doe";
+
+  @Autowired
+  private FhirContext fhirContext;
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void testUnsuccessfulOutcomeReturnedForInvalidFhirJson() throws Exception {
+    final String testCaseJson = "{ }";
+    FhirContext contextSpy = Mockito.spy(fhirContext);
+
+    mockMvc.perform(
+            MockMvcRequestBuilders.post("/fhir/validations/bundles")
+                .with(user(TEST_USER_ID))
+                .with(csrf())
+                .content(testCaseJson)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+        .andDo((result) -> assertThat(result.getResponse().getContentAsString(), is(notNullValue())))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.code").value(400))
+        .andExpect(jsonPath("$.successful").value(false));
+  }
+
+  @Test
+  void testUnsuccessfulOutcomeReturnedForBadResourceType() throws Exception {
+    final String testCaseJson = "{\"resourceType\": \"Patient\" }";
+
+    mockMvc.perform(
+            MockMvcRequestBuilders.post("/fhir/validations/bundles")
+                .with(user(TEST_USER_ID))
+                .with(csrf())
+                .content(testCaseJson)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+        .andDo((result) -> assertThat(result.getResponse().getContentAsString(), is(notNullValue())))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.code").value(400))
+        .andExpect(jsonPath("$.successful").value(false));
+  }
+
+  @Test
+  void testUnsuccessfulOutcomeReturnedForInvalidEncounter() throws Exception {
+    String tc1Json = getStringFromTestResource("/testCaseBundles/testCaseInvalidEncounter.json");
+
+    mockMvc.perform(
+            MockMvcRequestBuilders.post("/fhir/validations/bundles")
+                .with(user(TEST_USER_ID))
+                .with(csrf())
+                .content(tc1Json)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+        .andDo((result) -> assertThat(result.getResponse().getContentAsString(), is(notNullValue())))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.code").value(200))
+        .andExpect(jsonPath("$.successful").value(false));
+  }
+
+  @Test
+  void testUnsuccessfulOutcomeReturnedForValidTestCaseJson() throws Exception {
+    String tc1Json = getStringFromTestResource("/testCaseBundles/validTestCase.json");
+
+    mockMvc.perform(
+            MockMvcRequestBuilders.post("/fhir/validations/bundles")
+                .with(user(TEST_USER_ID))
+                .with(csrf())
+                .content(tc1Json)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+        .andDo((result) -> assertThat(result.getResponse().getContentAsString(), is(notNullValue())))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.code").value(200))
+        .andExpect(jsonPath("$.successful").value(true));
+  }
+
+}
