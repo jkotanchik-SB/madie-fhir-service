2fb16e6a53660a84950486c223e2b0da7497d3a0
nmorasb <nate.moraca@semanticbits.com>
2024-10-22 10:02:46 -0400

    MAT-7736: updates to the fhir factory to remove unnecessary logging and streamline some functions

--- src/main/java/gov/cms/madie/madiefhirservice/factories/ModelAwareFhirFactory.java
+++ src/main/java/gov/cms/madie/madiefhirservice/factories/ModelAwareFhirFactory.java
@@ -9,6 +9,7 @@ import gov.cms.madie.madiefhirservice.exceptions.UnsupportedTypeException;
 import gov.cms.madie.models.common.ModelType;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -20,18 +21,15 @@ public class ModelAwareFhirFactory {
   private final Map<String, FhirValidator> fhirValidatorMap;
   private final Map<String, FhirContext> fhirContextMap;
 
+  @Autowired
   public ModelAwareFhirFactory(
       Map<String, FhirValidator> fhirValidatorMap, Map<String, FhirContext> fhirContextMap) {
     this.fhirValidatorMap = fhirValidatorMap;
     this.fhirContextMap = fhirContextMap;
-    log.info("fhirValidatorMap: {}", fhirValidatorMap);
-    log.info("fhirContextMap: {}", fhirContextMap);
   }
 
   public FhirValidator getValidatorForModel(ModelType modelType) {
     FhirValidator validator = fhirValidatorMap.get(modelType.getShortValue() + "NpmFhirValidator");
-    log.info("getValidatorForModel: {}", modelType.getShortValue() + "NpmFhirValidator");
-    log.info("validators: {}", fhirValidatorMap);
     if (validator == null) {
       throw new UnsupportedTypeException(this.getClass().getName(), modelType.toString());
     }
@@ -40,8 +38,6 @@ public class ModelAwareFhirFactory {
   }
 
   public FhirContext getContextForModel(ModelType modelType) {
-    final String lookup = modelType.getShortValue() + "FhirContext";
-    log.info("lookup is: {}", lookup);
     FhirContext context = fhirContextMap.get(modelType.getShortValue() + "FhirContext");
     if (context == null) {
       throw new UnsupportedTypeException(this.getClass().getName(), modelType.toString());
@@ -52,6 +48,10 @@ public class ModelAwareFhirFactory {
   public IParser getParserForModel(ModelType modelType) {
     FhirContext context = getContextForModel(modelType);
 
+    if (context == null) {
+      throw new UnsupportedTypeException(this.getClass().getName(), modelType.toString());
+    }
+
     return context
         .newJsonParser()
         .setParserErrorHandler(new StrictErrorHandler())
@@ -59,33 +59,24 @@ public class ModelAwareFhirFactory {
   }
 
   /**
-   * Convenience method to encapsulate model-specific parsing as the parseResource call
-   * requires a concrete class.
-   * QDM ModelType is not supported.
+   * Convenience method to encapsulate model-specific parsing as the parseResource call requires a
+   * concrete class. QDM ModelType is not supported.
+   *
    * @param modelType
    * @param bundleString
    * @return
    * @throws DataFormatException
    * @throws ClassCastException
    */
-  public IBaseBundle parseForModel(ModelType modelType, String bundleString)
-      throws DataFormatException, ClassCastException {
-    FhirContext context = fhirContextMap.get(modelType.getShortValue() + "FhirContext");
-    if (context == null) {
-      throw new UnsupportedTypeException(this.getClass().getName(), modelType.toString());
-    }
+  public IBaseBundle parseForModel(ModelType modelType, String bundleString) {
+    IParser parser = this.getParserForModel(modelType);
 
-    IParser parser =
-        context
-            .newJsonParser()
-            .setParserErrorHandler(new StrictErrorHandler())
-            .setPrettyPrint(true);
-
-    IBaseBundle bundle = null;
+    IBaseBundle bundle;
     switch (modelType) {
-      case QDM_5_6 -> throw new UnsupportedTypeException("FhirValidatorFactory", "QDM v5.6");
+      case QDM_5_6 -> throw new UnsupportedTypeException(this.getClass().getName(), "QDM v5.6");
       case QI_CORE, QI_CORE_6_0_0 ->
           bundle = parser.parseResource(org.hl7.fhir.r4.model.Bundle.class, bundleString);
+      default -> throw new UnsupportedTypeException(this.getClass().getName(), "N/A");
     }
     return bundle;
   }
